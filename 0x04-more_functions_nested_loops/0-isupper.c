60bd01a85b04ccf4426225e435b486d6592241ed
moha9119 <mohamedsow58@gmail.com>
2022-07-07 14:58:52 -0700

    function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -15,13 +15,13 @@
 int _isupper(int c)
 {
 
-	if (c >= 65 && c <= 90)
+	while (c >= 65 && c <= 90)
 	{
 		return (1);
-	}
-	else if(c >= 97 && c <= 122)
-	{
-		return (0);
+		if (c >= 97 && c <= 122)
+		{
+			return (0);
+		}
 	}
 	_putchar(10);
 
