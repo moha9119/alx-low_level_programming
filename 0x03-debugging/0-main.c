294207149259faeee61e2962b8eb75acd53dbadc
moha9119 <mohamedsow58@gmail.com>
2022-07-08 14:12:40 -0700

    his main file is a test for a postitive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Check if function is correct
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int b;
+
+	b = 0;
+
+
+	positive_or_negative(b);
+
+	return (0);
+}
+
