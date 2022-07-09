be80c94d44cd4621f9089e8025dab61e5b24827c
moha9119 <mohamedsow58@gmail.com>
2022-07-08 19:28:18 -0700

    Fizz-Buzz test” is an interview question designed

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - multiple Fizz-Buzz
+ * prints the numbers from 1 to 100
+ * multiples of three print Fizz
+ * multiples of five print Buzz
+ * multiples of both three and five print FizzBuzz.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("Fizz Buzz");
+			printf(" ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf ("Fiz");
+			 printf(" ");
+		}
+		else
+		{
+			printf("%d", n);
+			printf(" ");
+		}
+	}
+	
+	printf("\n");
+
+	return (0);
+}
+
+
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,5 @@
 6. The shortest distance between two points is a straight line
 7. I feel like I am diagonally parked in a parallel universe
 
+8. You are so much sunshine in every square inch
+9. Fizz-Buzz
