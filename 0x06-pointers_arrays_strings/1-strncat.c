b9f81fd98226219f3b3239ba46779f660855b9a8
moha9119 <mohamedsow58@gmail.com>
2022-07-14 15:35:03 -0700

    vim

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strcat - concactenates
+ *
+ * @dest: pointer type char
+ * @src: pointer type char
+ * @n: integer
+ *
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, b = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (b < n)
+	{
+		*(dest + i) = *(src + b);
+		if (src[b] == '\0')
+		{
+			break;
+		}
+		i++;
+		b++;
+	}
+
+	return (dest);
+}
