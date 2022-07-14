84979ccb09c719007b38450f040ef1633780606b
moha9119 <mohamedsow58@gmail.com>
2022-07-14 14:58:22 -0700

    function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strcat - concactenates
+ *
+ * @dest: pointer type char
+ * @src: pointer type char
+ *
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, b = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (b >= 0)
+	{
+		dest[i] = src[b];
+		if (src[b] == '\0')
+		{
+			break;
+		}
+		i++;
+		b++;
+	}
+
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void melangeString(char *point);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char* strcpy(char* copieDeLaChaine, const char* chaineACopier);
+char *_strcat(char *dest, char *src);
+#endif
