d6c3ec3d8c26b9172eaab45f2052c3551d9795a1
moha9119 <mohamedsow58@gmail.com>
2022-08-03 13:39:31 -0700

    function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search
+ * @size: the number of ellement
+ * @cmp: pointer to the function
+ * Return: nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, a = 1;
+
+	if (array && size && cmp)
+	{
+		if (size <= 0)
+		{
+			return (a);
+		}
+
+		for (; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
+			{
+				a = i;
+				break;
+			}
+			if (cmp(array[i]) == -1)
+			{
+				return (a);
+			}
+		}
+	}
+
+	return (a);
+}
